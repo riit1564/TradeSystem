ab54db4d7bdd61195ce5532d36de44ed56fabaef
Mehta <rushabhmehta05@gmail.com>
2016-06-18 17:39:46 -0700

    First ever check-in of what's going to be glorious history...

---
+++ Lambda-Functions/Test/SiteCheckScheduler.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+from datetime import datetime
+from urllib2 import urlopen
+
+SITE = 'https://www.amazon.com/'  # URL of the site to check
+EXPECTED = 'Online Shopping'  # String expected to be on the page
+
+
+def validate(res):
+    '''Return False to trigger the canary
+
+    Currently this simply checks whether the EXPECTED string is present.
+    However, you could modify this to perform any number of arbitrary
+    checks on the contents of SITE.
+    '''
+    return EXPECTED in res
+
+
+def lambda_handler(event, context):
+    print('Checking {} at {}...'.format(SITE, event['time']))
+    try:
+        if not validate(urlopen(SITE).read()):
+            raise Exception('Validation failed')
+    except:
+        print('Check failed!')
+        raise
+    else:
+        print('Check passed!')
+        return event['time']
+    finally:
+        print('Check complete at {}'.format(str(datetime.now())))
